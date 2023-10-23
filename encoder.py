d2fd5edb5372b63a0570549b91c45346ee58de46
btabman <tarkana@ufl.edu>
2023-10-23 16:16:51 -0500

    added main menu and option actions

--- encoder.py
+++ encoder.py
@@ -24,4 +24,26 @@ def decode(sequence):
     
     
 
-
+if __name__ == '__main__':
+    option = 0
+    while option != 3:
+        menu()
+        option = int(input("Please enter an option: "))
+        code = ''
+        encoded = ''
+        
+        if option == 1:
+            code = input("Please enter your password to encode: ")
+            try:
+                test = int(code)
+                encoded = encode(code)
+                print("Your password has been encoded and stored!\n")
+            except:
+                print("Error: Only enter integers\n")
+        
+        if option == 2:
+            decoded = decode(encoded)
+            if str(decoded) != code:
+                print("Error: Decoded value does not match original code")
+            output = "The encoded password is {}, and the original password is {}.".format(encoded, decoded)
+            print(output)
